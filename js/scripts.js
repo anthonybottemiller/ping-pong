9c274e139010086a2a3ecbf4a6578ad275af7827
Anthony Bottemiller <anthonybottemiller@outlook.com>
2016-08-19 09:52:16 -0700

    Adds function countToNumber it currently tests whether number is negative responding with a error message

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
+var countToNumber = function(number){
+  if (number < 0){
+    return "Please use a positive integer"
+  }
+}
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
     console.log("Button Clicked");
     var inputNumber = $("input").val();
     console.log(inputNumber);
+    console.log(countToNumber(inputNumber));
   });
 });
