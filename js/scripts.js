820a6b34d99911a49c5d88e67abde61b61cb0cbf
Anthony Bottemiller <anthonybottemiller@outlook.com>
2016-08-19 09:56:26 -0700

    Creates additional functionality for countToNumber it now counts to number

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,11 @@ var countToNumber = function(number){
   if (number < 0){
     return "Please use a positive integer"
   }
+  else{
+    for (i=1; i <= number; i++){
+      console.log(i)
+    }
+  }
 }
 $(document).ready(function(){
   $("form").submit(function(event){
