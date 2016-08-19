4cf950584a6a0f72f1be2716816b7c5bed743c64
Anthony Bottemiller <anthonybottemiller@outlook.com>
2016-08-19 12:23:37 -0700

    adds styles to the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 var countToNumber = function(number){
+  outputArray.length = 0;
   if (number < 0){
     outputArray.push("Please use a positive integer");
   }
@@ -21,10 +22,10 @@ var countToNumber = function(number){
   numberOfResults += 1;
 };
 var writeDiv = function(){
-  $("#results").prepend('<div id=result'+numberOfResults+' class ="col-md-3"><ul></ul></div>');
+  $(".results").prepend('<div id=result'+numberOfResults+' class = "single-result col-md-2"><ul></ul></div>');
 };
 var writeHeader = function(){
-  $("#result"+numberOfResults).append("<h3>Result: "+numberOfResults+"</h3>");
+  $("#result"+numberOfResults).prepend("<h3>Result: "+numberOfResults+"</h3>");
 };
 var writeToResult = function(outputNumber){
   $("#result"+numberOfResults).append("<li>"+outputNumber+"</li>");
@@ -32,6 +33,7 @@ var writeToResult = function(outputNumber){
 var writeToPage = function(array){
   writeDiv();
   writeHeader();
+  console.log(outputArray)
   for (i=0; i<outputArray.length; i++){
     writeToResult(outputArray[i]);
   };
